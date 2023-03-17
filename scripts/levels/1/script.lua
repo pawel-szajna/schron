446549ea9135403acc7a5fc14d3a3443e5cb0aa9
Pawel Szajna <szajnapawel@gmail.com>
2023-03-17 23:55:46 +0100

    level: introduce more light sources

--- scripts/levels/1/script.lua
+++
@@ -1,5 +0,0 @@
-local textWindow = ui_createText()
-
-function enter_0_2_0()
-    ui_textWrite(textWindow, "Elo, witam w schronie", "KellySlab", 32)
-end
--- src/game/mode_in_game.cpp
+++ src/game/mode_in_game.cpp
@@ -53,13 +53,6 @@ std::optional<GameMode> ModeInGame::frame(double frameTime)
 {
     const uint8_t* keys = sdl::keyboard();
 
-    auto& lamp = const_cast<world::Sprite&>(world.level(1).sector(2).sprites[0]);
-    auto& light = const_cast<world::Light&>(world.level(1).sector(2).lights[0]);
-    lamp.x += direction * frameTime;
-    light.x += direction * frameTime;
-    if (lamp.x < -1.5) direction = 1;
-    if (lamp.x > 1.5) direction = -1;
-
     if (keys[SDL_SCANCODE_Q] or keys[SDL_SCANCODE_ESCAPE])
     {
         return GameMode::Quit;
