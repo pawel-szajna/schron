c52c759bc2b68693bfe583a0328f041c97801dba
Pawel Szajna <szajnapawel@gmail.com>
2023-03-21 07:24:27 +0100

    fix: gcc12 compilation

--- src/util/format.hpp
+++ src/util/format.hpp
@@ -7,9 +7,9 @@
 namespace std
 {
 template<typename ...T>
-std::string format(fmt::format_string<T...> fmt, T &&...args)
+std::string format(fmt::format_string<T...> fmt, T&&... args)
 {
-    return ::fmt::format(fmt, args...);
+    return ::fmt::format(fmt, std::forward<T>(args)...);
 }
 }
 #endif
