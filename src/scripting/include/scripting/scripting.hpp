c1dda863e3acd99cec9901b30bbb12c3926b9630
Pawel Szajna <szajnapawel@gmail.com>
2023-04-11 06:52:10 +0200

    docs: add Scripting documentation

--- src/scripting/include/scripting/scripting.hpp
+++ src/scripting/include/scripting/scripting.hpp
@@ -24,6 +24,15 @@ namespace scripting
 class UiBindings;
 class WorldBindings;
 
+/**
+ * @class Scripting
+ * @brief Bridge between the game and Lua scripts.
+ *
+ * This class acts as the main unit connecting the C++ game code to the Lua scripting
+ * framework. It provides an interface to the sol2 library it uses underneath.
+ *
+ * This class also automatically registers some C++-Lua bindings.
+ */
 class Scripting
 {
 public:
@@ -31,10 +40,35 @@ public:
     Scripting(ui::UI& ui, world::World& world, sdl::Renderer& renderer);
     ~Scripting();
 
+    /**
+     * @brief Starts a script.
+     * @param script Script filename
+     *
+     * Launches a Lua script in the current context.
+     */
     void run(const std::string& script);
+
+    /**
+     * @brief Starts a coroutine.
+     * @param script Script filename
+     *
+     * Launches a Lua script as a coroutine. Scripts which pause execution,
+     * waiting for results from C++, have to be launched as coroutines.
+     */
     void runAsCoroutine(const std::string& script);
+
+    /**
+     * @brief Resumes the current coroutine.
+     *
+     * This function does nothing (apart from printing a warning message) if
+     * there is no current coroutine.
+     */
     void resume();
 
+    /**
+     * @brief Removes a value or function binding from the current Lua context.
+     * @param name Binding name
+     */
     void unbind(const std::string& name);
 
     void sectorEntry(int x, int y, int z);
@@ -42,12 +76,27 @@ public:
 
     void save(std::ostream& os) const;
 
+    /**
+     * @brief Binds a function.
+     * @param name Function name, as seen in Lua environment
+     * @param args The function to bind, optionally with argument values to bind
+     */
     template<typename... Args>
     void bind(const std::string& name, Args&& ...args)
     {
         lua.set_function(name, std::forward<Args>(args)...);
     }
 
+    /**
+     * @brief Binds a function which yields execution.
+     * @param name Function name, as seen in Lua environment
+     * @param fn The function to bind
+     * @param args Optionally, argument values to bind
+     *
+     * This function is similar to Scripting::bind, however the Lua script execution
+     * will be paused after the function is called and has to be manually resumed by
+     * calling the Scripting::resume function.
+     */
     template<typename Fn, typename... Args>
     void bindYielding(const std::string& name, Fn&& fn, Args&& ...args)
     {
